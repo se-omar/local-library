d8d9528ed72db426cfc5f29d10726a4fd96db8c3
Omar Ayman <se.omarayman@gmail.com>
2023-03-10 18:12:16 +0200

    populated genre model

---
+++ models/genre.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const GenreSchema = new Schema({
+  name: {
+    type: String,
+    minLength: 3,
+    maxLength: 100,
+    required: true,
+  },
+});
+
+GenreSchema.virtual("url").get(function () {
+  return `/catalog/genre/${this._id}`;
+});
+
+export default mongoose.Schema("Genre", GenreSchema);
