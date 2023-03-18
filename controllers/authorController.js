81445e017e6fdab742a9d4732f1503a7c756c240
Omar Ayman <se.omarayman@gmail.com>
2023-03-18 17:52:21 +0200

    implemented createAuthor view and controller

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,3 +1,4 @@
+import { body, validationResult } from "express-validator";
 import Author from "../models/author.js";
 import Book from "../models/book.js";
 
@@ -40,14 +41,67 @@ export const authorDetail = async (req, res, next) => {
 
 // Display Author create form on GET.
 export const authorCreateGet = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+  res.render("authorForm", {
+    title: "Create Author",
+  });
 };
 
 // Handle Author create on POST.
-export const authorCreatePost = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author create POST");
-};
+export const authorCreatePost = [
+  // Validate and sanitize fields.
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
 
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified.")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+
+  body("date_of_death", "Invalid date of death")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.render("author_form", {
+        title: "Create Author",
+        author: req.body,
+        errors: errors.array(),
+      });
+    }
+
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+    });
+
+    try {
+      return author.save().then(() => {
+        res.redirect(author.url);
+      });
+    } catch (err) {
+      return next(err);
+    }
+  },
+];
 // Display Author delete form on GET.
 export const authorDeleteGet = (req, res) => {
   res.send("NOT IMPLEMENTED: Author delete GET");
